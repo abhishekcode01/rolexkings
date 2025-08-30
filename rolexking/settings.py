929ffd11b0661be87a308cecaef22a22a05cf113
abhishekcode01 <abhi1rajput676@gmail.com>
2025-08-30 18:02:42 +0530

    url updated

--- rolexking/settings.py
+++ rolexking/settings.py
@@ -25,11 +25,11 @@ SECRET_KEY = 'django-insecure-bxh(h63jcf8h3zlbm=9ls--u02m8(t0a!&p_fx7fa-gvpa5fen
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rolexsattaking.onrender.com']
+ALLOWED_HOSTS = ['https://shaktibazarsattaking.onrender.com']
 # ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://rolexsattaking.onrender.com',
+    'https://shaktibazarsattaking.onrender.com',
 ]
 
 
